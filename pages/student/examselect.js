f37b0f6a9d344b4d5727bbc30cd7208b9e5816c8
BINAY KUMAR SINHA <binaykumarsinha@BINAYs-MacBook-Air.local>
2023-08-06 22:09:41 +0530

    Changes for exam card integration to api

--- pages/student/examselect.js
+++ pages/student/examselect.js
@@ -60,11 +60,29 @@ export default function ExamSelect() {
       date: "07/08/2023"
     },
   ]
+
   const router = useRouter();
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const url = "https://exambackend-khqy.onrender.com/api/student/examFromUser/wilpproctor@gmail.com";
+    const bearerToken = "YOUR_BEARER_TOKEN";
+
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${bearerToken}`
+      }
+    })
+    .then(response => response.json())
+    .then(data => setData(data["examsForStudent"]))
+    .catch(error => console.error('Error fetching data:', error));
+  }, []);
+  console.log('data',data);
   return (
     <>
       <Header />
-      <div
+      {data?  <div
         style={{
           display: "flex",
           width: "100%",
@@ -72,7 +90,7 @@ export default function ExamSelect() {
           justifyContent: "center",
         }}
       >
-      {arr.map((ele)=>{
+      {data.map((ele)=>{
         return(
           <>
           <div onClick={() => {
@@ -83,7 +101,7 @@ export default function ExamSelect() {
           }}
           // onClick={fetchDataFromAPI}
           >
-          <Card company={ele.company} subject={ele.subject} exam_type={ele.exam_type} time={ele.time} date={ele.date}/></div>
+          <Card company={ele.company} subject={ele.subject} exam_type={ele.exam_type} time={ele.time+" minutes"} date={ele.date}/></div>
             {/* <div style={{display: "flex", flexDirection: "row"}}>
               <div>{ele.company}</div>
               <div>{ele.company}</div>
@@ -94,7 +112,10 @@ export default function ExamSelect() {
           </>
         )
          
-      })}
+      })}</div>:<div>Loading...</div>}
+      </>
+  )
+    }
 {/* 
         <div
           style={{
@@ -267,9 +288,216 @@ export default function ExamSelect() {
           </div>
           <div style={{marginLeft: "25%", marginTop: "5%"}}>Avail NOW!!</div>
         </div>*/}
-      </div> 
+      {/* </div> : <div>Loding...</div>}
+      <div
+        style={{
+          display: "flex",
+          width: "100%",
+          flexWrap: "wrap",
+          justifyContent: "center",
+        }}
+      > */}
+{/* 
+        <div
+          style={{
+            fontSize: "30px",
+            fontWeight: 600,
+            marginRight: "20px",
+            marginTop: "20px",
+            border: "6px solid #092d4f",
+            borderRadius: "2px",
+            padding: "20px",
+            width: "500px",
+            backgroundColor: "#2e8ee8",
+            color: "white",
+            boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+          }}
+        >
+          <div
+            style={{
+              marginLeft: "40%",
+              marginTop: "5%",
+            }}
+          >
+            TACO
+          </div>
+          <div
+            style={{ marginLeft: "auto", marginRight: "auto", width: "25%", marginTop: "5%" }}
+          >
+            Calculus
+          </div>
+          <div style={{marginLeft: "40%", marginTop: "5%"}}>EC2</div>
+          <div style={{ display: "flex" }}>
+            <div style={{marginLeft: "10%", marginTop: "5%"}}>IST 10:00am</div>
+            <div style={{marginLeft: "20%", marginTop: "5%"}}>23/07/2023</div>
+          </div>
+        </div>
+        <div
+          style={{
+            fontSize: "30px",
+            fontWeight: 600,
+            marginRight: "20px",
+            marginTop: "20px",
+            border: "6px solid #092d4f",
+            borderRadius: "2px",
+            padding: "20px",
+            width: "500px",
+            backgroundColor: "#2e8ee8",
+            color: "white",
+            boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+            cursor: "pointer",
+          }}
+          onClick={() => {
+            router.push({
+                pathname: "/student/examstart",
+                //query: { returnUrl: router.asPath },
+              });
+          }}
+        >
+          <div
+            style={{
+              marginLeft: "40%",
+              marginTop: "5%",
+            }}
+          >
+            TACO
+          </div>
+          <div
+            style={{ marginLeft: "30%", marginRight: "10%", width: "60%", marginTop: "5%" }}
+          >
+            Engineering Materials
+          </div>
+          <div style={{marginLeft: "40%", marginTop: "5%"}}>EC2</div>
+          <div style={{ display: "flex" }}>
+            <div style={{marginLeft: "0%", marginTop: "5%"}}>IST 10:00am</div>
+            <div style={{marginLeft: "20%", marginTop: "5%"}}>23/07/2023</div>
+          </div>
+        </div>
+        <div
+          style={{
+            fontSize: "30px",
+            fontWeight: 600,
+            marginRight: "20px",
+            marginTop: "20px",
+            border: "6px solid #092d4f",
+            borderRadius: "2px",
+            padding: "20px",
+            width: "500px",
+            backgroundColor: "#2e8ee8",
+            color: "white",
+            boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+          }}
+        >
+          <div
+            style={{
+              marginLeft: "40%",
+              marginTop: "5%",
+            }}
+          >
+            TACO
+          </div>
+          <div
+            style={{ marginLeft: "25%", marginRight: "15%", width: "70%", marginTop: "5%" }}
+          >
+            Elec. & Electronics Tech.
+          </div>
+          <div style={{marginLeft: "40%", marginTop: "5%"}}>EC2</div>
+          <div style={{ display: "flex" }}>
+            <div style={{marginLeft: "0%", marginTop: "5%"}}>IST 10:00am</div>
+            <div style={{marginLeft: "20%", marginTop: "5%"}}>23/07/2023</div>
+          </div>
+        </div>
+        <div
+          style={{
+            fontSize: "30px",
+            fontWeight: 600,
+            marginRight: "20px",
+            marginTop: "20px",
+            border: "6px solid #092d4f",
+            borderRadius: "2px",
+            padding: "20px",
+            width: "500px",
+            backgroundColor: "#2e8ee8",
+            color: "white",
+            boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+          }}
+        >
+          <div
+            style={{
+              marginLeft: "40%",
+              marginTop: "5%",
+            }}
+          >
+            TACO
+          </div>
+          <div
+            style={{ marginLeft: "15%", marginRight: "5%", width: "70%", marginTop: "5%" }}
+          >
+            Mechanics Of Solids
+          </div>
+          <div style={{marginLeft: "40%", marginTop: "5%"}}>EC2</div>
+          <div style={{ display: "flex" }}>
+            <div style={{marginLeft: "0%", marginTop: "5%"}}>IST 10:00am</div>
+            <div style={{marginLeft: "20%", marginTop: "5%"}}>23/07/2023</div>
+          </div>
+        </div>
+        <div
+          style={{
+            fontSize: "30px",
+            fontWeight: 600,
+            marginRight: "20px",
+            marginTop: "20px",
+            border: "6px solid #092d4f",
+            borderRadius: "2px",
+            padding: "20px",
+            width: "500px",
+            backgroundColor: "#2e8ee8",
+            color: "white",
+            boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+          }}
+        >
+          <div
+            style={{
+              marginLeft: "25%",
+              marginTop: "5%",
+            }}
+          >
+            Practice Tests
+          </div>
+          <div style={{ marginLeft: "40%", marginTop: "5%" }}>
+            (10)
+          </div>
+          <div style={{marginLeft: "25%", marginTop: "5%"}}>Avail NOW!!</div>
+        </div>*/}
+      {/* </div> 
 
       <Footer />
     </>
   );
-}
+} */}
+
+
+
+// {arr.map((ele)=>{
+//   return(
+//     <>
+//     <div onClick={() => {
+//       router.push({
+//           pathname: "/student/examstart",
+//           //query: { returnUrl: router.asPath },
+//         });
+//     }}
+//     // onClick={fetchDataFromAPI}
+//     >
+//     <Card company={ele.company} subject={ele.subject} exam_type={ele.exam_type} time={ele.time} date={ele.date}/></div>
+//       {/* <div style={{display: "flex", flexDirection: "row"}}>
+//         <div>{ele.company}</div>
+//         <div>{ele.company}</div>
+//         <div>{ele.company}</div>
+//         <div>{ele.company}</div>
+//         <div>{ele.company}</div>
+//       </div> */}
+//     </>
+//   )
+   
+// })}
