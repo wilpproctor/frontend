af6f979fc19a5aec0464d822f50af603a8af8ff3
apple <apple@apples-MacBook-Air.local>
2023-08-11 20:46:58 +0530

    disconnecting student and proctor from socket once logged out

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import Link from "next/link";
 import StudentContext from "../lib/StudentContext";
 import axios from 'axios';
